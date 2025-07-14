41d8dda28dc165bfa7e2af0ef7271b1ef1ec9b94
sreedevi-04 <sreedevipatil2000@gmail.com>
2025-07-14 13:12:14 +0530

    Initial commit

---
+++ src/main/java/page1.java
@@ -0,0 +1,7 @@
+public class page1 {
+    public static void main(String[] args){
+        int a=100, b=200;
+        System.out.println(a);
+        System.out.println( "The sum of a and b is:"+(a+b));
+    }
+}
