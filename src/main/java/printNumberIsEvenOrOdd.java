7493be133251e28378a54527cb3eb66552b11259
sreedevi-04 <sreedevipatil2000@gmail.com>
2025-07-14 23:23:19 +0530

    printNumberIsEvenOrOdd

---
+++ src/main/java/printNumberIsEvenOrOdd.java
@@ -0,0 +1,15 @@
+public class printNumberIsEvenOrOdd {
+    public static void main(String[] args) {
+        int i=10;
+        while(i<=20){
+            int a=0;
+            if(i%2==0){
+                System.out.println(i+" is  Even number");
+            }
+            else {
+                System.out.println(i+" is Odd Number");
+            }
+            i++;
+        }
+    }
+}
