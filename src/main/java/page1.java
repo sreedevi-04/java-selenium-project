41cc7bb4e8705d423cdcbac4037887f5b3fe74ce
sreedevi-04 <sreedevipatil2000@gmail.com>
2025-07-14 13:16:21 +0530

    new comment added

--- src/main/java/page1.java
+++ src/main/java/page1.java
@@ -1,7 +1,3 @@
-public class page1 {
-    public static void main(String[] args){
-        int a=100, b=200;
-        System.out.println(a);
-        System.out.println( "The sum of a and b is:"+(a+b));
-    }
-}
+public class page1
+
+// this comment is to test
