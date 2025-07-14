7cb951aefbc1fe15edd42fd7793567b134d1da16
sreedevi-04 <sreedevipatil2000@gmail.com>
2025-07-14 23:48:09 +0530

    doWhileLoop

---
+++ src/main/java/doWhileLoop.java
@@ -0,0 +1,12 @@
+public class doWhileLoop {
+    public static void main(String[] args) {
+        //if the condition is not sastified the statement will execute atleast once in do while loop
+        //where as in while loop, if the condition is not sastified the statement will not execute even for once
+        int i=10;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i<=5);
+    }
+}
