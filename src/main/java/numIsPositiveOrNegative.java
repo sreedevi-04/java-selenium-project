45fe622c137b1d341620127b287519e9fa70c7d4
sreedevi-04 <sreedevipatil2000@gmail.com>
2025-07-14 16:54:21 +0530

    numIsPositiveOrNegative

---
+++ src/main/java/numIsPositiveOrNegative.java
@@ -0,0 +1,15 @@
+public class numIsPositiveOrNegative {
+     public static void main(String[]args){
+          int num=-63;
+          if(num>0){
+               System.out.print(num+ " is a positive number");
+          }
+          else if(num<0) {
+               System.out.print(num+ " is a negative number");
+          }
+          else{
+               System.out.print(num+ "it's a zero");
+          }
+
+     }
+}
