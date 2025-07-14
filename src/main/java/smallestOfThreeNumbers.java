f3b51796e80da241c4dfb0b8c239cb7c096612f3
sreedevi-04 <sreedevipatil2000@gmail.com>
2025-07-14 22:10:10 +0530

    smallestOfThreeNumbers

---
+++ src/main/java/smallestOfThreeNumbers.java
@@ -0,0 +1,16 @@
+public class smallestOfThreeNumbers
+{
+    public static void main(String[] args) {
+        int a=10, b=5, c=0;
+        if(a<b){
+            System.out.println(a);
+        }
+        else if(b<c){
+            System.out.println(b);
+        }
+        else {
+            System.out.println(c);
+        }
+
+    }
+}
