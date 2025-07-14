004d01fdc4e9c56001b1b5d13176915d1b1f190a
sreedevi-04 <sreedevipatil2000@gmail.com>
2025-07-14 14:25:20 +0530

    added swaping of numbers

--- src/main/java/page1.java
+++ src/main/java/page1.java
@@ -1,4 +1,15 @@
-public class page1
+public class page1 {
+    public static void main(String[] args){
+        int a=100, b=200;
+        System.out.println(a);
+        System.out.println( "The sum of a and b is:"+(a+b));
+        int e=10, f=20;
+        e=f+e;
+        f=e-f;
+        e=e-f;
+         System.out.println(e);
+        System.out.println(f);
 
-// this comment is to test
-// add and makes changes
+
+    }
+}
