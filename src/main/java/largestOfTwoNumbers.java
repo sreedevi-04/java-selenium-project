da26f47e302aa0a8e30f4b91f29cdd46f9e4bc7f
sreedevi-04 <sreedevipatil2000@gmail.com>
2025-07-14 22:00:04 +0530

    largestOfTwoNumbers

--- src/main/java/largestOfTwoNumbers.java
+++ src/main/java/largestOfTwoNumbers.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class largestOfTwoNumbers {
+    public static void main(String[] args) {
+        int a=2,b=3;
+       int max=(a>b) ? a:b;
+        System.out.println(" the max value is:" + max);
+
+    }
+
 }
