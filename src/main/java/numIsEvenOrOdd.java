ddcbe06294daa6f891388c3d32ef19b619ece4fd
sreedevi-04 <sreedevipatil2000@gmail.com>
2025-07-14 15:38:30 +0530

    numIsEvenOrOdd

---
+++ src/main/java/numIsEvenOrOdd.java
@@ -0,0 +1,11 @@
+public class numIsEvenOrOdd {
+    public static void main(String[] args){
+        int a=77;
+        if(a%2==0){
+            System.out.print(a+" is a even number");}
+            else{
+            System.out.print(a+" is a odd number");
+
+        }
+    }
+}
---
+++ src/main/java/org/example/ifCondition.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ifCondition {
+    public static void main(String[]args){
+        int age=10;
+        if(age>=18){
+            System.out.println( age+" age is valid ");
+        }
+        else {
+            System.out.println(age+" age is not valid");
+        }
+    }
+}
