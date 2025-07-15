4d41abc2f1cbbce733f6a466b5167a0dbabc07b2
sreedevi-04 <sreedevipatil2000@gmail.com>
2025-07-15 10:59:03 +0530

    breakStatement

---
+++ src/main/java/forLoopStatement.java
@@ -0,0 +1,12 @@
+public class forLoopStatement {
+    public static void main(String[] args) {
+        for(int i=10;i>=0;i--){
+            if(i%2==0){
+            System.out.println(i+" is Even number");
+            }
+            else {
+                System.out.println(i+ " is odd number");
+            }
+        }
+    }
+}
