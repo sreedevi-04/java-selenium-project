457eb73150454b7fb70147fad1bd2f0ecc306a9e
sreedevi-04 <sreedevipatil2000@gmail.com>
2025-07-21 01:00:09 +0530

    ReadingAndWritingDataIntoArray

--- src/main/java/ReadingAndWritingDataIntoArray.java
+++ src/main/java/ReadingAndWritingDataIntoArray.java
@@ -1,2 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ReadingAndWritingDataIntoArray {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int[]a=new int[5];
+        for( int i=0; i<a.length;i++){
+            System.out.println("Enter a value for the position "+ i+" :");
+            a[i]=sc.nextInt();
+        }
+        System.out.println("Array elements are:");
+        System.out.println(Arrays.toString(a));
+    }
 }
