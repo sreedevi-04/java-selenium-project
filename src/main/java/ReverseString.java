6ce613b3e1e27e718f793b5e2187a0d2f013c438
sreedevi-04 <sreedevipatil2000@gmail.com>
2025-07-22 17:55:13 +0530

    ReverseString

--- src/main/java/ReverseString.java
+++ src/main/java/ReverseString.java
@@ -1,11 +1,20 @@
 public class ReverseString {
     public static void main(String[] args) {
-        String s="selenium" ;
-        String rev=" ";
+        String s = "selenium";
+        /*String rev=" ";
         for( int i=s.length()-1;i>=0;i--){
              rev=rev +s.charAt(i);
         }
         System.out.println(rev);
 
+    }*/
+        // Approach 2 by converting string to character array
+        char[] a = s.toCharArray();
+        //System.out.println(a);
+        String rev=" ";
+        for(int i=a.length-1;i>=0;i--){
+           rev= rev + a[i];
+        }
+        System.out.println(rev);
     }
 }
