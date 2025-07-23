acbadc6355915294bc45083705fa582e1db070a7
sreedevi-04 <sreedevipatil2000@gmail.com>
2025-07-23 11:13:19 +0530

    RemoveJunkCharacter

--- src/main/java/RemoveJunkCharacter.java
+++ src/main/java/RemoveJunkCharacter.java
@@ -1,2 +1,15 @@
 public class RemoveJunkCharacter {
+    public static void main(String[] args) {
+        String s="query1234";
+        //s.replace()*/
+        //String input = "T@a!l a#t123";
+
+        // Keep only letters and digits [^a-zA-Z0-9]
+        String cleaned = s.replaceAll("[^a-zA-Z0-9]", "");
+
+        System.out.println("Original String: " + s);
+        System.out.println("Cleaned String: " + cleaned);
+
+
+    }
 }
