cf6151a164094a17cd9ea631ce9872bd8db2b850
sreedevi-04 <sreedevipatil2000@gmail.com>
2025-07-23 11:34:24 +0530

    RemoveJunkCharacter

---
+++ src/main/java/RemoveSpaceInString.java
@@ -0,0 +1,7 @@
+public class RemoveSpaceInString {
+    public static void main(String[] args) {
+        String s="W e l c o m e";
+       String news= s.replaceAll(" ", "");
+        System.out.println(news);
+    }
+}
